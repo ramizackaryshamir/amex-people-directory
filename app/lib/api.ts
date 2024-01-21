1f489de4dd957d658b3c0e083718977199e8c0b6
MeetYourCreator <rzshamir@icloud.com>
2024-01-21 15:22:53 -0500

    set up api to fetch data from randomuser.me

---
+++ app/lib/api.ts
@@ -0,0 +1,20 @@
+const host = 'https://randomuser.me'
+export const api = {
+
+
+  getRandomUsers: async () => {
+    const response = await fetch(`${host}/api/?results=10`, {
+      method: 'GET',
+      // cache: 'no-cache'
+    });
+      if (!response.ok) {
+      throw new Error('Failed to fetch data');
+      }
+    
+    const data = await response.json();
+    const results = data.results
+    const seed = data.info.seed
+  return {results, seed};
+  }
+
+}
